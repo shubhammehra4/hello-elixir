42e25581a4cfd2b3adb389a59ece53d8fbfd5306
Shubham Mehra <shubhammehra4@gmail.com>
2021-06-01 18:56:17 +0530

    word_count

---
+++ lib/word_count.exs
@@ -0,0 +1,7 @@
+IO.gets("File to count the words from: ")
+|> String.trim()
+|> File.read!()
+|> String.split(~r{(\\n|[^\w'])+})
+|> Enum.filter(fn x -> x != "" end)
+|> Enum.count()
+|> IO.inspect()
